91a2a54f397c930fb1708f28471be462af30c963
Олександр Марченко <80917411+aomarchenko@users.noreply.github.com>
2021-12-02 01:53:22 +0200

    Update feedback.js

--- src/components/feedback/feedback.js
+++ src/components/feedback/feedback.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 class Feedback extends React.Component {
-  //
+  static defaultProps = {
+    initialValue: 0,
+  };
+
   state = {
-    goodValue: 0,
-    neutralValue: 0,
-    badValue: 0,
+    goodValue: this.props.initialValue,
+    neutralValue: this.props.initialValue,
+    badValue: this.props.initialValue,
+    totalValue: this.props.initialValue,
+    everageGoodValue: this.props.initialValue,
   };
 
   makeGoodFeedback = () => {
@@ -24,6 +29,33 @@ class Feedback extends React.Component {
       return { badValue: prevState.badValue + 1 };
     });
   };
+
+  countTotalFeedback = () => {
+    this.setState(prevState => {
+      return {
+        totalValue: this.state.goodValue + this.state.badValue + this.state.neutralValue + 1,
+      };
+    });
+    console.log(this.state.goodValue + this.state.badValue + this.state.neutralValue + 1);
+  };
+  countPositiveFeedbackPercentage = () => {
+    if (this.state.goodValue !== 0) {
+      this.setState(prevState => {
+        return {
+          everageGoodValue:
+            (100 / (this.state.goodValue + this.state.badValue + this.state.neutralValue + 1)) *
+            (this.state.goodValue + 1),
+        };
+      });
+    }
+
+    console.log(
+      100 /
+        (this.state.goodValue + this.state.badValue + this.state.neutralValue + 1) /
+        (this.state.goodValue + 0.000000000000000000000000000000000000000000001 + 1),
+    );
+    // console.log(this.state.goodValue);
+  };
   render() {
     return (
       <div className="Feedback">
@@ -33,6 +65,8 @@ class Feedback extends React.Component {
             type="button"
             onClick={event => {
               this.makeGoodFeedback();
+              this.countTotalFeedback();
+              this.countPositiveFeedbackPercentage();
             }}
           >
             Good
@@ -41,6 +75,8 @@ class Feedback extends React.Component {
             type="button"
             onClick={event => {
               this.makeNeutralFeedback();
+              this.countTotalFeedback();
+              this.countPositiveFeedbackPercentage();
             }}
           >
             Neutral:
@@ -49,6 +85,8 @@ class Feedback extends React.Component {
             type="button"
             onClick={() => {
               this.makeBadFeedback();
+              this.countTotalFeedback();
+              this.countPositiveFeedbackPercentage();
             }}
           >
             Bad
@@ -59,6 +97,8 @@ class Feedback extends React.Component {
           <span className="FeedbackResults">Neutral: {this.state.neutralValue}</span>
           <span className="FeedbackResults">Bad: {this.state.badValue}</span>
         </div>
+        <p>Total Feedbacks: {this.state.totalValue}</p>
+        <p>Everage Good Values {this.state.everageGoodValue}%</p>
       </div>
     );
   }
